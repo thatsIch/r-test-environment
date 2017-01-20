45b474e1f9729c71e6ec5c75cc32b8e4bf14108d
thatsIch <thatsIch@mail.de>
2017-01-20 16:47:26 +0100

    Add simple tests

---
+++ R/math.R
@@ -0,0 +1,4 @@
+sum <- function(first_summand, second_summand) {
+  # return
+  first_summand + second_summand
+}
---
+++ tests/testthat/test-math.R
@@ -0,0 +1,8 @@
+context("math.R")
+
+## TODO: Rename context
+## TODO: Add more tests
+
+test_that("sum works", {
+  expect_equal(sum(10, 15), 25)
+})
